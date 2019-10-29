f090dd49edcb58323d7a834dd931e060073fe560
Ryanlawton001 <56266188+Ryanlawton001@users.noreply.github.com>
2019-10-29 12:37:24 +0000

    Add files via upload

--- SwapTest.java
+++ SwapTest.java
@@ -1,5 +1,7 @@
 package Swap;
 
+import org.junit.jupiter.api.Test;
+
 import java.lang.reflect.Array;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,14 +11,11 @@ class SwapTest {
         String[] originalString = new String[]{"Mike", "Rupert", "Charles", "Barry"};
         String[] updatedString = new String[]{"Mike", "Barry", "Charles", "Rupert"};
         assertArrayEquals(updatedString, Swap.swap(originalString, 1, 3));
-
     }
 
-    private void testSwap(Array array, int index , int index2){
-        Array[] test = new Array[5];
-        Swap swap = new Swap();
-        assertArrayEquals();
-
+    @Test
+    private void testSwap(){
+        testSwapString();
     }
 
 }
