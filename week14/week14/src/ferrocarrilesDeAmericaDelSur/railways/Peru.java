46f9b42bf78b1364a5913c514a6a58ed23a5692a
Ryanlawton001 <56266188+Ryanlawton001@users.noreply.github.com>
2020-02-04 13:36:09 +0000

    Add files via upload

---
+++ week14/week14/src/ferrocarrilesDeAmericaDelSur/railways/Peru.java
@@ -0,0 +1,47 @@
+package ferrocarrilesDeAmericaDelSur.railways;
+
+import ferrocarrilesDeAmericaDelSur.errors.RailwaySystemError;
+import ferrocarrilesDeAmericaDelSur.errors.SetUpError;
+import ferrocarrilesDeAmericaDelSur.tools.Clock;
+import ferrocarrilesDeAmericaDelSur.tools.Delay;
+
+/**
+ * An implementation of a railway.  The runTrain method, should, in collaboration with Bolivia's runTrain(), guarantee
+ * safe joint operation of the railways.
+ */
+public class Peru extends Railway {
+	/**
+	 * Change the parameters of the Delay constructor in the call of the superconstructor to
+	 * change the behaviour of this railway.
+	 *
+	 * @throws SetUpError if there is an error in setting up the delay.
+	 */
+	public Peru() throws SetUpError {
+		super("Peru", new Delay(0.1, 0.3));
+	}
+
+	/**
+	 * Run the train on the railway.
+	 * This method currently does not provide any synchronisation to avoid two trains being in the pass at the same time.
+	 */
+	public void runTrain() throws RailwaySystemError {
+		Clock clock = getRailwaySystem().getClock();
+		Railway nextRailway = getRailwaySystem().getNextRailway(this);
+		while (!clock.timeOut()) {
+			choochoo();
+			getBasket().putStone();
+
+			while (nextRailway.getBasket().hasStone()) {
+				if(getBasket().hasStone() == nextRailway.getBasket().hasStone()){
+					getBasket().takeStone();
+
+					while (!getBasket().hasStone());
+					getBasket().putStone();
+				}
+			}
+			crossPass();
+			nextRailway.getBasket().putStone();
+			getBasket().takeStone();
+		}
+	}
+}
